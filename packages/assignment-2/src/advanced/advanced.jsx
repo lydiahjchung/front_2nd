51509b23221c9cc5d03f84f352cac990855c0811
Lydia <lydia@suppliesfitness.com>
2024-06-26 01:57:52 +0900

    fix: useCustomState

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,5 +1,8 @@
 import { createContext, useContext, useState } from 'react';
 
+// 실제로 값이 달라졌는지 확인하기 위해 사용
+import { deepEquals } from '../basic/basic';
+
 // basic - CustomNumber와 비슷하게 map을 사용함으로
 // map에 이미 값이 있는지 중복확인하기 = 값을 캐시하기
 const cache = new Map();
@@ -25,7 +28,18 @@ export const memo2 = (fn, dependencies) => {
 };
 
 export const useCustomState = (initValue) => {
-  return useState(initValue);
+  // initValue as state
+  const [state, setState] = useState(initValue);
+
+  // setState 함수만 바꿔주기:
+  // when new value comes in, check if that value is deepEqual to current value
+  // if it is, don't update the state
+  const customSetState = (newValue) => {
+    if (deepEquals(newValue, state)) return;
+    setState(newValue);
+  };
+
+  return [state, customSetState];
 };
 
 const textContextDefaultValue = {
