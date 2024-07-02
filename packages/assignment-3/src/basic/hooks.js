f0187a40c69c762bf40ecf7303b9b1b6ad79d34e
Lydia <lydia@suppliesfitness.com>
2024-07-03 00:59:34 +0900

    fix: hook.js

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,15 +1,45 @@
+import { deepEquals } from '../../../assignment-2/src/basic/basic';
+
 export function createHooks(callback) {
+  let states = [];
+  let stateIndex = -1;
+
+  let memoCache = [];
+  let memoCacheIndex = -1;
+
   const useState = (initState) => {
-    return [];
+    states.push(initState);
+
+    stateIndex += 1;
+    const currentStateIndex = stateIndex;
+    const currentState = states[currentStateIndex];
+
+    const setState = (newState) => {
+      if (!deepEquals(states[currentStateIndex], newState)) {
+        states[currentStateIndex] = newState;
+        callback();
+      }
+    };
+
+    return [currentState, setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    memoCache.push({ fn: fn, refs: refs });
+    memoCacheIndex += 1;
+    const currentMemoIndex = memoCacheIndex;
+
+    if (!deepEquals(memoCache[currentMemoIndex].refs, refs)) {
+      memoCache[currentMemoIndex] = { fn: fn, refs: refs };
+    }
+
+    return memoCache[currentMemoIndex].fn;
   };
 
   const resetContext = () => {
-
-  }
+    stateIndex = -1;
+    memoCacheIndex = -1;
+  };
 
   return { useState, useMemo, resetContext };
 }
