78fdc1e6fc0a99c3f9fffb0c0fb19bfefe613d0b
Lydia <lydia@suppliesfitness.com>
2024-07-12 04:10:07 +0900

    fix: refactor basic

--- packages/assignment-4/src/basic/main.basic.js
+++ packages/assignment-4/src/basic/main.basic.js
@@ -1,131 +1,88 @@
+import {
+  createDOMElement,
+  appendChildElement,
+  appendSelectOptions,
+  setAttribute,
+} from "./render";
+import {
+  findProduct,
+  getCartItemText,
+  getDiscountRateById,
+  calculateDiscountRate,
+  MAX_DISCOUNT_RATE,
+} from "./utils";
+
 const PRODUCTS = [
   { id: "p1", name: "상품1", price: 10000 },
   { id: "p2", name: "상품2", price: 20000 },
   { id: "p3", name: "상품3", price: 30000 },
 ];
 
-const DISCOUNT_RATE = {
-  p1: 0.1,
-  p2: 0.15,
-  p3: 0.2,
-};
-
-const MIN_DISCOUNT_QUANTITY = 10;
-const MAX_DISCOUNT_QUANTITY = 30;
-const MAX_DISCOUNT_RATE = 0.25;
-
-const createDOMElement = (name, options) => {
-  const newElement = document.createElement(name);
-
-  if (options?.id) {
-    newElement.id = options.id;
-  }
-
-  if (options?.className) {
-    newElement.className = options.className;
-  }
-
-  if (options?.textContent) {
-    newElement.textContent = options.textContent;
-  }
-
-  if (options?.value) {
-    newElement.value = options.value;
-  }
-
-  if (options?.dataset) {
-    Object.entries(options.dataset).forEach(([key, value]) => {
-      newElement.dataset[key] = value;
-    });
-  }
-
-  return newElement;
-};
-
-const appendChildElement = (parent, ...children) => {
-  children.forEach((child) => {
-    parent.appendChild(child);
-  });
-};
-
-const addSelectOptionList = (parent, options) => {
-  options.forEach((option) => {
-    const newOption = createDOMElement("option", {
-      value: option.id,
-      textContent: `${option.name} - ${option.price}원`,
-    });
-    appendChildElement(parent, newOption);
-  });
-};
-
-const findProductById = (selectedProductId) => {
-  return PRODUCTS.find(({ id }) => id === selectedProductId);
-};
-
-const getCartItemText = (cartItem, index) => {
-  return cartItem.querySelector("span").textContent.split("x ")[index];
-};
-
-// const getQuantityByCartItem = (cartItem, index) => {
-//   return parseInt(getCartItemText(cartItem, index));
-// };
-
-const getDiscountRateById = (productId, quantity) => {
-  if (quantity < MIN_DISCOUNT_QUANTITY) {
-    return 0;
-  }
-
-  return DISCOUNT_RATE[productId];
-};
-
-const calculateDiscountRate = (quantity, prevTotal, totalPrice) => {
-  const bulkDiscount = totalPrice * MAX_DISCOUNT_RATE;
-  const individualDiscount = prevTotal - totalPrice;
-
-  return quantity >= MAX_DISCOUNT_QUANTITY && bulkDiscount > individualDiscount
-    ? MAX_DISCOUNT_RATE
-    : (prevTotal - totalPrice) / prevTotal;
-};
-
 function main() {
   const app = document.getElementById("app");
-  const container = createDOMElement("div", { className: "bg-gray-100 p-8" });
-  appendChildElement(app, container);
+  const container = createDOMElement({
+    name: "div",
+    options: { className: "bg-gray-100 p-8" },
+  });
+  appendChildElement({
+    parent: app,
+    children: [container],
+  });
 
-  const cartContainer = createDOMElement("div", {
-    className:
-      "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8",
+  const cartContainer = createDOMElement({
+    name: "div",
+    options: {
+      className:
+        "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8",
+    },
   });
-  appendChildElement(container, cartContainer);
+  appendChildElement({
+    parent: container,
+    children: [cartContainer],
+  });
 
-  const title = createDOMElement("h1", {
-    className: "text-2xl font-bold mb-4",
-    textContent: "장바구니",
+  const title = createDOMElement({
+    name: "h1",
+    options: { className: "text-2xl font-bold mb-4", textContent: "장바구니" },
   });
-  const cartItemsContainer = createDOMElement("div", { id: "cart-items" });
-  const cartTotalContainer = createDOMElement("div", {
-    id: "cart-total",
-    className: "text-xl font-bold my-4",
+  const cartItemsContainer = createDOMElement({
+    name: "div",
+    options: { id: "cart-items" },
   });
-  const addButton = createDOMElement("button", {
-    id: "add-to-cart",
-    className: "bg-blue-500 text-white px-4 py-2 rounded",
-    textContent: "추가",
+  const cartTotalContainer = createDOMElement({
+    name: "div",
+    options: {
+      id: "cart-total",
+      className: "text-xl font-bold my-4",
+    },
   });
-  const productSelect = createDOMElement("select", {
-    id: "product-select",
-    className: "border rounded p-2 mr-2",
+  const addButton = createDOMElement({
+    name: "button",
+    options: {
+      id: "add-to-cart",
+      className: "bg-blue-500 text-white px-4 py-2 rounded",
+      textContent: "추가",
+    },
   });
-  appendChildElement(
-    cartContainer,
-    title,
-    cartItemsContainer,
-    cartTotalContainer,
-    productSelect,
-    addButton,
-  );
+  const productSelect = createDOMElement({
+    name: "select",
+    options: {
+      id: "product-select",
+      className: "border rounded p-2 mr-2",
+    },
+  });
+  appendChildElement({
+    parent: cartContainer,
+    children: [
+      title,
+      cartItemsContainer,
+      cartTotalContainer,
+      productSelect,
+      addButton,
+    ],
+  });
 
-  addSelectOptionList(productSelect, PRODUCTS);
+  appendSelectOptions({ select: productSelect, options: PRODUCTS });
 
   const updateCart = () => {
     let totalPrice = 0;
@@ -133,37 +90,50 @@ function main() {
     let prevTotal = 0;
     let discountRate = 0;
 
-    const cartItems = cartItemsContainer.childNodes;
-
-    cartItems.forEach((cartItem) => {
-      const product = findProductById(cartItem.id);
-      const quantity = parseInt(getCartItemText(cartItem, 1));
+    Array.from(cartItemsContainer.children).forEach((cartItem) => {
+      const product = findProduct({
+        products: PRODUCTS,
+        selectedId: cartItem.id,
+      });
+      const quantity = parseInt(getCartItemText({ cartItem, index: 1 }));
       const total = product.price * quantity;
-      const discount = getDiscountRateById(product.id, quantity);
+      const discount = getDiscountRateById({ productId: product.id, quantity });
 
       totalQuantity += quantity;
       prevTotal += total;
       totalPrice += total * (1 - discount);
     });
 
-    discountRate = calculateDiscountRate(totalQuantity, prevTotal, totalPrice);
+    discountRate = calculateDiscountRate({
+      quantity: totalQuantity,
+      prevTotal,
+      totalPrice,
+    });
 
     if (discountRate === MAX_DISCOUNT_RATE) {
       totalPrice = prevTotal * (1 - MAX_DISCOUNT_RATE);
     }
 
-    cartTotalContainer.textContent = `총액: ${Math.round(totalPrice)}원`;
+    setAttribute({
+      element: cartTotalContainer,
+      attributes: {
+        textContent: `총액: ${Math.round(totalPrice)}원`,
+      },
+    });
 
     if (discountRate > 0) {
-      // totalPrice = prevTotal * (1 - discountRate);
-      // cartTotalContainer.textContent = `총액: ${Math.round(totalPrice)}원`;
-
-      const discountSpan = createDOMElement("span", {
-        className: "text-green-500 ml-2",
-        textContent: `(할인 ${(discountRate * 100).toFixed(1)}%)`,
+      const discountSpan = createDOMElement({
+        name: "span",
+        options: {
+          className: "text-green-500 ml-2",
+          textContent: `(${(discountRate * 100).toFixed(1)}% 할인 적용)`,
+        },
       });
 
-      appendChildElement(cartTotalContainer, discountSpan);
+      appendChildElement({
+        parent: cartTotalContainer,
+        children: [discountSpan],
+      });
     }
   };
 
@@ -178,57 +148,80 @@ function main() {
     });
 
     if (selectedProduct) {
-      const element = document.getElementById(selectedProduct.id);
-      if (element) {
-        element.querySelector("span").textContent = `${
-          selectedProduct.name
-        } - ${selectedProduct.price}원 x ${
-          parseInt(getCartItemText(element, 1)) + 1
-        }`;
+      const cartItem = document.getElementById(selectedProduct.id);
+      if (cartItem) {
+        const quantity = parseInt(getCartItemText({ cartItem, index: 1 })) + 1;
+        setAttribute({
+          element: cartItem.querySelector("span"),
+          attributes: {
+            textContent: `${selectedProduct.name} - ${selectedProduct.price}원 x ${quantity}`,
+          },
+        });
       } else {
-        const divContainer = createDOMElement("div", {
-          id: selectedProduct.id,
-          className: "flex justify-between items-center mb-2",
+        const divContainer = createDOMElement({
+          name: "div",
+          options: {
+            id: selectedProduct.id,
+            className: "flex justify-between items-center mb-2",
+          },
         });
-        appendChildElement(cartItemsContainer, divContainer);
+        appendChildElement({
+          parent: cartItemsContainer,
+          children: [divContainer],
+        });
 
-        const spanContainer = createDOMElement("span", {
-          textContent: `${selectedProduct.name} - ${selectedProduct.price}원 x 1`,
+        const spanContainer = createDOMElement({
+          name: "span",
+          options: {
+            textContent: `${selectedProduct.name} - ${selectedProduct.price}원 x 1`,
+          },
         });
-        const buttonContainer = createDOMElement("div");
-        appendChildElement(divContainer, spanContainer, buttonContainer);
+        const buttonContainer = createDOMElement({
+          name: "div",
+        });
+        appendChildElement({
+          parent: divContainer,
+          children: [spanContainer, buttonContainer],
+        });
 
-        const decreaseButton = createDOMElement("button", {
-          className:
-            "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1",
-          textContent: "-",
-          dataset: {
-            productId: selectedProduct.id,
-            change: -1,
+        const decreaseButton = createDOMElement({
+          name: "button",
+          options: {
+            className:
+              "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1",
+            textContent: "-",
+            dataset: {
+              productId: selectedProduct.id,
+              change: -1,
+            },
           },
         });
-        const increaseButton = createDOMElement("button", {
-          className:
-            "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1",
-          textContent: "+",
-          dataset: {
-            productId: selectedProduct.id,
-            change: 1,
+        const increaseButton = createDOMElement({
+          name: "button",
+          options: {
+            className:
+              "quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1",
+            textContent: "+",
+            dataset: {
+              productId: selectedProduct.id,
+              change: 1,
+            },
           },
         });
-        const removeButton = createDOMElement("button", {
-          className: "remove-item bg-red-500 text-white px-2 py-1 rounded",
-          textContent: "삭제",
-          dataset: {
-            productId: selectedProduct.id,
+        const removeButton = createDOMElement({
+          name: "button",
+          options: {
+            className: "remove-item bg-red-500 text-white px-2 py-1 rounded",
+            textContent: "삭제",
+            dataset: {
+              productId: selectedProduct.id,
+            },
           },
         });
-        appendChildElement(
-          buttonContainer,
-          decreaseButton,
-          increaseButton,
-          removeButton,
-        );
+        appendChildElement({
+          parent: buttonContainer,
+          children: [decreaseButton, increaseButton, removeButton],
+        });
       }
       updateCart();
     }
@@ -246,13 +239,17 @@ function main() {
 
       if (target.classList.contains("quantity-change")) {
         const change = parseInt(target.dataset.change);
-        const quantity = parseInt(getCartItemText(cartItem, 1)) + change;
+        const quantity =
+          parseInt(getCartItemText({ cartItem, index: 1 })) + change;
 
         if (quantity > 0) {
-          cartItem.querySelector("span").textContent = `${getCartItemText(
-            cartItem,
-            0,
-          )} x ${quantity}`;
+          const cartItemText = getCartItemText({ cartItem, index: 0 });
+          setAttribute({
+            element: cartItem.querySelector("span"),
+            attributes: {
+              textContent: `${cartItemText} x ${quantity}`,
+            },
+          });
         } else {
           cartItem.remove();
         }
