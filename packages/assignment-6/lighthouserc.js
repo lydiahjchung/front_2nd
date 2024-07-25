8ee916c09a9d123aafcb4e2e75c93830676c145e
Lydia <lydia@suppliesfitness.com>
2024-07-25 20:26:22 +0900

    fix: lighthouserc

---
+++ packages/assignment-6/lighthouserc.js
@@ -0,0 +1,15 @@
+export default {
+  ci: {
+    collect: {
+      staticDistDir: "./", // Use current directory
+      url: ["index.html"], // Test the simple HTML file
+      numberOfRuns: 1,
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+    },
+  },
+}
