7f3552498d453a9f800633c11a8421d348a16a5e
Lydia <lydia@suppliesfitness.com>
2024-07-25 21:10:01 +0900

    fix: lighthouse

--- packages/assignment-6/.lighthouserc.cjs
+++ packages/assignment-6/.lighthouserc.cjs
@@ -1,18 +1,13 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./",
-      url: ["index.html"],
+      staticDistDir: "./dist",
+      // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+      url: ["http://localhost:5173"],
       numberOfRuns: 1,
-      settings: {
-        chromeFlags: "--no-sandbox --disable-gpu --disable-dev-shm-usage",
-      },
     },
     upload: {
       target: "temporary-public-storage",
-    },
-    assert: {
-      preset: "lighthouse:no-pwa",
     },
   },
 }
